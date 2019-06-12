Fixed MSE calculation

model.py
@@ -3,7 +3,9 @@ from utils import (
   train_input_setup,
   test_input_setup,
   save_params,
-  array_image_save
+  array_image_save,
+  mse_models
+
 )
 
 import time
@@ -193,6 +195,21 @@ class Model(object):
       ax3 = fig.add_subplot(133); ax3.imshow(result.reshape(384,384)); ax3.axis('off'); ax3.set_title('Predicted')
 
       plt.savefig('./result/' + str(k) + '_test.png')
+
+      # Calculate Test MSE
+    all = mse_models(self)
+    mse_all = []
+    for img in all:
+        test_data = img[0]
+        test_label = img[1]
+        result = np.clip(self.pred.eval({self.images: test_data, self.labels: test_label, self.batch: 1}), 0, 1)
+        result = result.reshape(384,384)
+        test_label = test_label.reshape(384,384)
+        mse = np.sum((np.abs(result.astype("float") - test_label.astype("float"))) ** 2)
+        mse /= float(result.shape[0] * result.shape[1])
+        mse_all.append(mse)
+    print("##########################")
+    print("MSE IS: ", np.mean(mse_all))
 
   def save(self, step):
     model_name = self.model.name + ".model"

utils.py
@@ -12,16 +12,23 @@ from multiprocessing import Pool, Lock, active_children
 import pickle
 FLAGS = tf.app.flags.FLAGS
 
+
 def single_image_mse(number):
+    # BICUBIC INTERPOLATIONS MSE
     import joblib
     import cv2
-    img1 = joblib.load('./x_test_hr.pickle')[number]
-    img2 = joblib.load('./lr.pickle')['x_test_lr'][number]
-    img2 = cv2.resize(img2,(384,384), interpolation = cv2.INTER_CUBIC)
+    hr = joblib.load('./x_test_hr.pickle')
+    lr = joblib.load('./lr.pickle')['x_test_lr']
+    mse_all= []
+    for img1,img2 in zip(lr,hr):
 
-    mse = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
-    mse /= float(img1.shape[0] * img1.shape[1])
-    return mse
+        img1 = cv2.resize(img1,(384,384), interpolation = cv2.INTER_CUBIC)
+
+        mse = np.sum((np.abs(img2.astype("float") - img1.astype("float"))) ** 2)
+        mse /= float(img2.shape[0] * img2.shape[1])
+        mse_all.append(mse)
+    print("MSE BICUBIC: ", np.mean(mse_all))
+    return np.mean(mse_all)
 
 def train_input_setup(config):
   """
@@ -65,6 +72,31 @@ def train_input_setup(config):
 
   return (arrdata, arrlabel)
 
+def mse_models(config):
+    with open('lr.pickle', 'rb') as pickle_in:
+      lr = pickle.load(pickle_in)
+
+    x_test_hr = joblib.load('x_test_hr.pickle')
+    x_test_lr = lr["x_test_lr"]
+    del lr
+
+    all = []
+    for input_, label_ in zip(x_test_lr, x_test_hr):
+
+        if len(input_.shape) == 3:
+          h, w, _ = input_.shape
+        else:
+          h, w = input_.shape
+
+        arrdata = input_.reshape([1, h, w, 1])
+        if len(label_.shape) == 3:
+          h, w, _ = label_.shape
+        else:
+          h, w = label_.shape
+
+        arrlabel = label_.reshape([1, h, w, 1])
+        all.append((arrdata, arrlabel))
+    return all
 
 def test_input_setup(config, number):
 